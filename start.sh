e8820544b066fb51ae2a6948753fd511f9a31431
cyber-tatarin <dmitriyseur@gmail.com>
2024-01-18 19:16:50 +0300

    add flask requirements

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 alembic upgrade head
 pip install -r requirements.txt
-#uvicorn asgi:asgi_app web.main:app
-#python -m web.main &
-uvicorn asgi:asgi_app --host 0.0.0.0 --port $PORT &
+python -m web.main &
 python main.py
 
