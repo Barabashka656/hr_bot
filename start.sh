dba424ddc6edeb8cb3124b7821b6132d12c1c427
cyber-tatarin <dmitriyseur@gmail.com>
2024-01-18 15:35:43 +0300

    update poetry

--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# Install Poetry
-curl -sSL https://install.python-poetry.org | python3.10
-
-# Add Poetry to PATH
-export PATH="$HOME/.local/bin:$PATH"
-
-poetry install
-poetry run alembic upgrade head
-poetry run python main.py
+alembic upgrade head
+pip install -r requirements.txt
+python main.py
