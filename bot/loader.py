9cb5b7913180a63fa777168e7ff73cd3f963b84f
cyber-tatarin <dmitriyseur@gmail.com>
2024-01-22 19:20:44 +0300

    add redis storage

--- bot/loader.py
+++ bot/loader.py
@@ -1,11 +1,15 @@
-from bot.data.config import REDIS_HOST, REDIS_PORT, TELEGRAM_API_TOKEN, OPEN_AI_API_KEY
+from aiogram.fsm.storage.redis import RedisStorage
+
+from bot.data.config import REDIS_HOST, REDIS_PORT, TELEGRAM_API_TOKEN, OPEN_AI_API_KEY, REDIS_URL
 
 from aiogram import Bot
 from aiogram import Dispatcher
 from openai import AsyncOpenAI
 
 bot = Bot(token=TELEGRAM_API_TOKEN)
-dp = Dispatcher()
+storage = RedisStorage.from_url(REDIS_URL)
+dp = Dispatcher(storage=storage)
+
 
 openai_client = AsyncOpenAI(
     api_key=OPEN_AI_API_KEY,
--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 alembic upgrade head
 pip install -r requirements.txt
-python -m web.main &
 python main.py
 
